d4a81206e579b593e5650c270f4e02fd0c857a9c
Shubh7amydv <shubham.2023ug1109@iiitranchi.ac.in>
2026-02-06 23:11:23 +0530

    Completed the Baisc http server along with folder setup

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+
+const { PORT }= require('./config/serverconfig')
+const apiRoutes=require('./Routes/index')
+
+const db=require('./models/index')
+
+const setupAndStartServer=()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('api',apiRoutes);
+    
+    app.listen(PORT,()=>{
+        console.log(`Port started at ${PORT}`);
+
+        if(process.env.DB_SYNC){
+                db.sequelize.sync({alter:true});
+        }
+    })
+}
+
+setupAndStartServer();
+
+
+
